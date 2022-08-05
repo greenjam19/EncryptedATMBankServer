c24f543c03950d6fab6e47c0636ce968e9dca9fd
Sean Stearns <66488781+Sean-Stearns@users.noreply.github.com>
2022-08-04 20:26:14 -0400

    Echo server implemented

--- server.py
+++ server.py
@@ -6,6 +6,23 @@ import socket
 def main():
 	#static variables
 	HOST = "127.0.0.1"
-	PORT = 65432
+	PORT = 42069
 
-	with socket.socket
+	# No need to call s.close() here, as everything is done in a with
+	# statement
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		# Using TCP                             ^^^^^^^^
+		#Binding to associate socket w/ localhost (127.0.0.1)
+		s.bind((HOST, PORT))
+		s.listen()
+
+		print("SERVER: Blocked on accept")
+		conn, addr = s.accept()
+		with conn:
+			print("SERVER: Connected to {}", addr)
+			while(1):
+				data = conn.recv(512)
+				if(!data):
+					break
+				print("SERVER: Received {}", data)
+				conn.sendall(data)
