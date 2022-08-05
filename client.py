0b727ac465c01d925d9e2f8ec950065913deeeed
Sean Stearns <66488781+Sean-Stearns@users.noreply.github.com>
2022-08-05 00:59:28 -0400

    Basic banking operations complete. Handshake next

--- client.py
+++ client.py
@@ -3,22 +3,64 @@
 #imports
 import socket
 
+def check_int(d_w):
+	st = "How much would you like to " + d_w + "?\n"
+	amount = input(st)
+	try:
+		amount_int = int(amount)
+		is_int = True
+	except ValueError:
+		is_int = False
+	return amount, is_int
+
 def main():
 	# static variables
 	HOST = "127.0.0.1"
 	PORT = 65432
 
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		# Authentication steps
 		s.connect((HOST, PORT))
+		
+		
 		# Entering a loop where we will continually communicate
 		# w/ the bank
 		while(1):
 			# Use sendall to send messages to server (bank)
 			print("USAGE: <Withdraw/Deposit/Check/Quit>")
 			print("")
-			message = input("What would you like to do?\n")
-			s.sendall(message.encode('UTF-8'))
+			message = input("What would you like to do?\n").lower()
+			transmission = ""
+			if(message == "check"):
+				print("CLIENT: Requested balance check")
+				transmission += "check "
+			elif(message == "deposit"):
+				print("CLIENT: Requested money deposit")
+				transmission += "deposit "
+				amount, is_int = check_int("deposit")
+				if not is_int:
+					print("WARNING: Please enter an integer value")
+					continue
+				transmission += amount
+			elif(message == "withdraw"):
+				print("CLIENT: Requested money withdraw")
+				transmission += "withdraw "
+				amount, is_int = check_int("withdraw")
+				if not is_int:
+					print("WARNING: Please enter an integer value")
+					continue
+				transmission += amount
+			elif(message == "quit"):
+				print("CLIENT: Atm requested quit. Exiting...")
+				transmission += "quit"
+			else:
+				print("WARNING: Unrecognized command")
+				continue
+
+			s.sendall(transmission.lower().encode('UTF-8'))
 			data = s.recv(512).decode()
+			if(data == "Quit"):
+				break
 			print("Received", data)
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -8,6 +8,9 @@ def main():
 	HOST = "127.0.0.1"
 	PORT = 65432
 
+	# non-static variables
+	balance = 0
+
 	# No need to call s.close() here, as everything is done in a with
 	# statement
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -25,8 +28,27 @@ def main():
 				if not data:
 					break
 				data = data.decode()
-				print("SERVER: Received", data)
-				conn.sendall(data.encode('UTF-8'))
+				lis = data.split()
+				returnable = ""
+				if(lis[0] == "quit"):
+					print("SERVER: Received quit request from ATM. Exiting...")
+					returnable += "Quit"
+				else:
+					print("SERVER: Received", data)
+					if(lis[0] == "deposit"):
+						balance += int(lis[1])
+						returnable += "Success"
+					elif(lis[0] == "withdraw"):
+						if(balance - int(lis[1]) < 0):
+							print("WARNING: Not enough capital in account to make withdrawl")
+							returnable += "Warning"
+						else:
+							balance +- int(lis[1])
+							returnable += "Success"
+					elif(lis[0] == "check"):
+						print("SERVER: Client has", balance, " dollars remaining in account")
+						returnable += "Success"
+				conn.sendall(returnable.encode("UTF-8"))
 
 if __name__ == "__main__":
 	main()
