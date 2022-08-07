c4da15cac0994962250400d0ddcdf0171322d3dd
Theod0reWu <43049406+Theod0reWu@users.noreply.github.com>
2022-08-07 18:17:42 -0400

    moved RSA to its own file

--- client.py
+++ client.py
@@ -4,18 +4,7 @@
 import socket
 import math
 
-def RSA_encrypt(public_key, message):
-	message = int.from_bytes(message.encode(), byteorder = 'little')
-	ciphertext = pow(message, public_key[0], public_key[1])
-	len = math.ceil(ciphertext.bit_length() / 8)
-	return ciphertext.to_bytes(len, byteorder = 'little')
-
-def RSA_decrypt(private_key, msg):
-	msg = int.from_bytes(msg, 'little', signed = False)
-	message = pow(msg, private_key[0], private_key[1] * private_key[2])
-	len = math.ceil(message.bit_length() / 8)
-	message = message.to_bytes(len, byteorder = 'little').decode()
-	return(message)
+from RSA import RSA_encrypt, RSA_decrypt
 
 def check_int(d_w):
 	st = "How much would you like to " + d_w + "?\n"
--- server.py
+++ server.py
@@ -6,18 +6,7 @@ import os
 import random
 import math
 
-def RSA_decrypt(private_key, msg):
-	msg = int.from_bytes(msg, 'little', signed = False)
-	message = pow(msg, private_key[0], private_key[1] * private_key[2])
-	len = math.ceil(message.bit_length() / 8)
-	message = message.to_bytes(len, byteorder = 'little').decode()
-	return(message)
-
-def RSA_encrypt(public_key, message):
-	message = int.from_bytes(message.encode(), byteorder = 'little')
-	ciphertext = pow(message, public_key[0], public_key[1])
-	len = math.ceil(ciphertext.bit_length() / 8)
-	return ciphertext.to_bytes(len, byteorder = 'little')
+from RSA import RSA_encrypt, RSA_decrypt
 
 def main():
 	#static variables
